08a2a2fa9e59b004b126cd3b7c13ac98760f01ee
Marilyn Wen <marilynwen2001@gmail.com>
2025-06-20 12:44:48 -0700

    initializing

---
+++ RecipiesTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RecipiesTest {
+    /*
+    @Test
+    public void calculatorInitialValueZero() {
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.getValue());
+    } */
+
+    
+}
